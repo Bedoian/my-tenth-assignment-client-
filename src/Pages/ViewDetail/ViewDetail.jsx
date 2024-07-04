93c8ea79d0df5fb44d041124934e2f27bf0e64f3
Rafioul Hasan <rafioulhasan2@gmail.com>
2024-07-04 20:24:06 +0600

    view detail section added

--- src/Pages/ViewDetail/ViewDetail.jsx
+++ src/Pages/ViewDetail/ViewDetail.jsx
@@ -2,7 +2,8 @@
 import { useLoaderData } from "react-router-dom";
 import LoIcon from '../../assets/icons8-location-64.png'
 import euro from '../../assets/icons8-money-96.png'
-import vacation from '../../assets/icons8-vacation-96.png'
+import map from '../../assets/icons8-country-96.png'
+import duration from '../../assets/icons8-time-100.png'
 const ViewDetail = () => {
     const detail = useLoaderData()
     const { name, country, location, photo, cost, Ttime, visitor, discription, season } = detail;
@@ -11,7 +12,7 @@ const ViewDetail = () => {
             <div className=" mt-10">
                 <div className=" flex gap-7">
                     <div className="  mb-6  rounded-lg">
-                        <img src={photo} className="lg:w-[800px] " />
+                        <img src={photo} className="lg:w-[800px] rounded-2xl " />
                     </div>
                     <div className="w-3/5 text-start">
                         <p className="text-3xl font-bold"></p>
@@ -32,22 +33,31 @@ const ViewDetail = () => {
                                 <table className="table">
                                     <tbody>
                                         <tr>
-                                            <td className="flex">
-                                                <img className="w-8 h-8" src={euro} alt="" />
-                                                <p className="relative top-1 text-blue-500 text-lg font-bold">€{cost}</p>
+                                            <td className="flex justify-between ">
+                                                <div className="flex relative top-2">
+                                                    <img className="w-8 h-8" src={euro} alt="" />
+                                                    <p className="relative top-1 text-blue-500 text-lg font-bold">€{cost}</p>
+                                                </div>
+                                                <div>
+                                                    <p className="bg-green-100 p-2 rounded-full text-lg font-semibold text-green-500">#{season} Vacation</p>
+                                                </div>
                                             </td>
                                             <td className="text-sm font-bold"></td>
                                         </tr>
                                         <tr>
-                                            <td className="text-sm text-gray-500 font-semibold">Publisher :</td>
-                                            <td className="text-sm font-bold"></td>
+                                            <td className="flex justify-between">
+                                                <div className="flex gap-1">
+                                                    <img className="w-5 h-5" src={duration} alt="" />
+                                                    <p className="text-[15px] relative bottom-[1px] text-gray-500 font-semibold">{Ttime}</p>
+                                                </div>
+                                                <div className="flex gap-1">
+                                                    <img  className="w-7 h-7 relative bottom-1" src={map} alt="" />
+                                                    <p className="text-[15px]  font-semibold text-red-500">{country}</p>
+                                                </div>
+                                            </td>
                                         </tr>
                                         <tr>
-                                            <td className="text-sm text-gray-500 font-semibold">Year of Publishing :</td>
-                                            <td className="text-sm font-bold"></td>
-                                        </tr>
-                                        <tr>
-                                            <td className="text-sm text-gray-500 font-semibold">Rating :</td>
+                                            <td className="text-center text-lg font-semibold">About {visitor} people visit yearly</td>
                                             <td className="text-sm font-bold" ></td>
                                         </tr>
                                     </tbody>
